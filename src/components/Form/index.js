22670262b3ed6f89aeb8ef75ead4a089c42ba876
AdnanShah <adnanshah9911@gmail.com>
2018-07-10 10:14:32 -0700

    form end

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,48 +1,64 @@
 import React from "react";
-
 import CardBox from "components/CardBox/index";
 import IntlMessages from "util/IntlMessages";
 import Input from "@material-ui/core/Input";
 import InputAdornment from "@material-ui/core/InputAdornment";
 import TextField from "@material-ui/core/TextField";
 import MenuItem from "@material-ui/core/MenuItem";
-import Checkbox from "@material-ui/core/Checkbox";
-
 import FormLabel from "@material-ui/core/FormLabel";
 import FormControl from "@material-ui/core/FormControl";
 import FormGroup from "@material-ui/core/FormGroup";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
-import FormHelperText from "@material-ui/core/FormHelperText";
+import Radio from "@material-ui/core/Radio";
+import Button from "@material-ui/core/Button";
 
-const ranges = [
+const level = [
   {
-    value: "0-20",
-    label: "0 to 20"
+    value: "Easy",
+    label: "Easy"
   },
   {
-    value: "21-50",
-    label: "21 to 50"
+    value: "Mid",
+    label: "Mid"
   },
   {
-    value: "51-100",
-    label: "51 to 100"
+    value: "Difficult",
+    label: "Difficult"
+  }
+];
+const category = [
+  {
+    value: "Science",
+    label: "Science"
+  },
+  {
+    value: "Islam",
+    label: "Islam"
+  },
+  {
+    value: "Other",
+    label: "Other"
   }
 ];
 class Form extends React.Component {
   state = {
-    amount: "",
-    password: "",
-    weight: "",
-    weightRange: "",
-    showPassword: false,
-    checkedA: true,
-    checkedB: true
+    question: "",
+    answer: "",
+    option1: "",
+    option2: "",
+    option3: "",
+    option4: "",
+    level: "",
+    category: "",
+    addCategory: false
   };
 
   handleChange = prop => event => {
     this.setState({ [prop]: event.target.value });
   };
-
+  addCategory = () => {
+    this.setState({ addCategory: !this.state.addCategory });
+  };
   render() {
     console.log("state", this.state);
     return (
@@ -54,10 +70,11 @@ class Form extends React.Component {
           >
             <div className="row">
               <TextField
+                className=""
                 id="question"
-                label="Question"
-                value={this.state.name}
-                onChange={this.handleChange("name")}
+                label="Enter Question Here"
+                value={this.state.question}
+                onChange={this.handleChange("question")}
                 margin="normal"
                 fullWidth
               />
@@ -70,11 +87,18 @@ class Form extends React.Component {
                     <FormControlLabel
                       control={
                         <div>
-                          <Checkbox
-                            checked={this.state.option1}
-                            onChange={this.handleChange("option1")}
+                          <Radio
+                            checked={this.state.answer === "option1"}
+                            onChange={this.handleChange("answer")}
                             value="option1"
+                            name="option1"
+                            aria-label="A"
                           />
+
+                          {/* <Checkbox
+                            onChange={this.handleChange("answer")}
+                            value="option1"
+                          /> */}
                           <TextField
                             id="option1"
                             label="Option#1"
@@ -89,11 +113,17 @@ class Form extends React.Component {
                     <FormControlLabel
                       control={
                         <div>
-                          <Checkbox
-                            checked={this.state.option2}
-                            onChange={this.handleChange("option2")}
-                            value="option1"
+                          <Radio
+                            checked={this.state.answer === "option2"}
+                            onChange={this.handleChange("answer")}
+                            value="option2"
+                            name="option2"
+                            aria-label="A"
                           />
+                          {/* <Checkbox
+                            onChange={this.handleChange("answer")}
+                            value="option1"
+                          /> */}
                           <TextField
                             id="option2"
                             label="Option#2"
@@ -108,11 +138,17 @@ class Form extends React.Component {
                     <FormControlLabel
                       control={
                         <div>
-                          <Checkbox
-                            checked={this.state.option3}
-                            onChange={this.handleChange("option3")}
+                          <Radio
+                            checked={this.state.answer === "option3"}
+                            onChange={this.handleChange("answer")}
                             value="option3"
+                            name="option3"
+                            aria-label="A"
                           />
+                          {/* <Checkbox
+                            onChange={this.handleChange("answer")}
+                            value="option3"
+                          /> */}
                           <TextField
                             id="option3"
                             label="Option#3"
@@ -127,11 +163,17 @@ class Form extends React.Component {
                     <FormControlLabel
                       control={
                         <div>
-                          <Checkbox
-                            checked={this.state.option4}
-                            onChange={this.handleChange("option4")}
+                          <Radio
+                            checked={this.state.answer === "option4"}
+                            onChange={this.handleChange("answer")}
                             value="option4"
+                            name="option4"
+                            aria-label="A"
                           />
+                          {/* <Checkbox
+                            onChange={this.handleChange("answer")}
+                            value="option4"
+                          /> */}
                           <TextField
                             id="option4"
                             label="Option#4"
@@ -152,34 +194,55 @@ class Form extends React.Component {
                 className="col-4 mt-5"
                 //   style={{ border: "2px solid red" }}
               >
-                <h1 className="text-danger text-center mb-2">Mandatory Options</h1>
+                <h1 className="text-danger text-center mb-2">
+                  Mandatory Options
+                </h1>
                 <div className="">
-                  <TextField
-                    select
-                    value={this.state.weightRange}
-                    onChange={this.handleChange("weightRange")}
-                    fullWidth
-                    InputProps={{
-                      startAdornment: (
-                        <InputAdornment position="start">
-                          Category
-                        </InputAdornment>
-                      )
-                    }}
+                  {this.state.addCategory == false ? (
+                    <TextField
+                      select
+                      value={this.state.category}
+                      onChange={this.handleChange("category")}
+                      fullWidth
+                      InputProps={{
+                        startAdornment: (
+                          <InputAdornment position="start">
+                            Category
+                          </InputAdornment>
+                        )
+                      }}
+                    >
+                      {category.map(option => (
+                        <MenuItem key={option.value} value={option.value}>
+                          {option.label}
+                        </MenuItem>
+                      ))}
+                    </TextField>
+                  ) : (
+                    <TextField
+                      fullWidth
+                      id="category"
+                      label="Category"
+                      value={this.state.category}
+                      onChange={this.handleChange("category")}
+                      margin="normal"
+                    />
+                  )}
+                  <Button
+                    variant="outlined"
+                    color="secondary"
+                    className="mt-2 text-center"
+                    onClick={this.addCategory}
                   >
-                    {ranges.map(option => (
-                      <MenuItem key={option.value} value={option.value}>
-                        {option.label}
-                      </MenuItem>
-                    ))}
-                  </TextField>
+                    Add Other Category
+                  </Button>
                   {/* </div>
                 <div className=""> */}
                   <TextField
                     select
                     className="mt-5"
-                    value={this.state.weightRange}
-                    onChange={this.handleChange("weightRange")}
+                    value={this.state.level}
+                    onChange={this.handleChange("level")}
                     fullWidth
                     InputProps={{
                       startAdornment: (
@@ -187,12 +250,19 @@ class Form extends React.Component {
                       )
                     }}
                   >
-                    {ranges.map(option => (
+                    {level.map(option => (
                       <MenuItem key={option.value} value={option.value}>
                         {option.label}
                       </MenuItem>
                     ))}
                   </TextField>
+                  <Button
+                    variant="contained"
+                    color="secondary"
+                    className="mt-2 text-center"
+                  >
+                    Add Question
+                  </Button>
                 </div>
               </div>
             </div>
