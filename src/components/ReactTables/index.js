4b1d4620181f2c6eb36280d17962b0ce5954c4a5
AdnanShah <adnanshah9911@gmail.com>
2018-07-11 08:31:50 -0700

    page 2 complete

--- src/components/ReactTables/index.js
+++ src/components/ReactTables/index.js
@@ -3,96 +3,9 @@ import IntlMessages from "util/IntlMessages";
 import Paper from "@material-ui/core/Paper";
 import axios from "axios";
 import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
-import ListItemText from "@material-ui/core/ListItemText";
-import Collapse from "@material-ui/core/Collapse";
-import InboxIcon from "@material-ui/icons/MoveToInbox";
-import ExpandLess from "@material-ui/icons/ExpandLess";
-import ExpandMore from "@material-ui/icons/ExpandMore";
-import StarBorder from "@material-ui/icons/StarBorder";
-const questions = [
-  {
-    id: 1,
-    question: "question#1",
-    options1: "options1",
-    options2: "options2",
-    options3: "options3",
-    options4: "options4"
-  },
-  {
-    id: 2,
-    question: "question#2",
-    options1: "",
-    options2: "",
-    options3: "",
-    options4: ""
-  },
-  {
-    id: 3,
-    question: "question#3",
-    options1: "",
-    options2: "",
-    options3: "",
-    options4: ""
-  },
-  {
-    id: 4,
-    question: "question#4",
-    options1: "",
-    options2: "",
-    options3: "",
-    options4: ""
-  },
-  {
-    id: 5,
-    question: "question#5",
-    options1: "",
-    options2: "",
-    options3: "",
-    options4: ""
-  },
-  {
-    id: 6,
-    question: "question#6",
-    options1: "",
-    options2: "",
-    options3: "",
-    options4: ""
-  },
-  {
-    id: 7,
-    question: "question#7",
-    options1: "",
-    options2: "",
-    options3: "",
-    options4: ""
-  },
-  {
-    id: 8,
-    question: "question#8",
-    options1: "",
-    options2: "",
-    options3: "",
-    options4: ""
-  },
-  {
-    id: 9,
-    question: "question#9",
-    options1: "",
-    options2: "",
-    options3: "",
-    options4: ""
-  },
-  {
-    id: 10,
-    question: "question#10",
-    options1: "",
-    options2: "",
-    options3: "",
-    options4: ""
-  }
-];
+import Collapsible from "react-collapsible";
+import Button from "@material-ui/core/Button";
+import { questions } from "./data";
 
 class ReactTables extends Component {
   constructor(props) {
@@ -102,16 +15,10 @@ class ReactTables extends Component {
       x_access_key: "ZVYC-9AA9-13QT-484S",
       x_access_token:
         "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MywiaWF0IjoxNTMwOTMyMzY2fQ.zqmGxDLhsKFomoYcDwFVArh5CZzSzJUrubHisOwEB80",
-      open: true,
       userArray: [],
       questions
     };
   }
-  handleClick = i => {
-    console.log("i", i);
-    console.log("questions", questions[i]);
-    this.setState(state => ({ open: !state.open }));
-  };
   componentWillMount() {
     console.log("in did mount");
     let data = this.state.x_access_key;
@@ -153,45 +60,51 @@ class ReactTables extends Component {
             styleName="col-lg-12 h-100"
             heading={<IntlMessages id="Question List:" />}
           >
+            <h1 className="ml-2 bold mb-5 mt-5 ">Question List</h1>
             {questions.map((item, i) => {
               return (
-                <List component="nav">
-                  <ListItem button onClick={this.handleClick.bind(this, i)}>
-                    <ListItemIcon>
-                      <InboxIcon />
-                    </ListItemIcon>
-                    <ListItemText inset primary={item.question} />
-                    {this.state.open ? <ExpandLess /> : <ExpandMore />}
-                  </ListItem>
-                  <Collapse in={this.state.open} timeout="auto" unmountOnExit>
-                    <List component="div" disablePadding>
-                      <ListItem button className="">
-                        <ListItemIcon>
-                          <StarBorder />
-                        </ListItemIcon>
-                        <ListItemText inset primary={item.options1} />
-                      </ListItem>
-                      <ListItem button className="">
-                        <ListItemIcon>
-                          <StarBorder />
-                        </ListItemIcon>
-                        <ListItemText inset primary={item.options2} />
-                      </ListItem>
-                      <ListItem button className="">
-                        <ListItemIcon>
-                          <StarBorder />
-                        </ListItemIcon>
-                        <ListItemText inset primary={item.options3} />
-                      </ListItem>
-                      <ListItem button className="">
-                        <ListItemIcon>
-                          <StarBorder />
-                        </ListItemIcon>
-                        <ListItemText inset primary={item.options4} />
-                      </ListItem>
+                <div className="mr-5 ml-5" style={{ lineHeight: "2px" }}>
+                  <Collapsible trigger={`Q#${i + 1}`}>
+                    <h1>{item.question}</h1>
+                    <List component="nav">
+                      <div className="row mt-2 mb-2">
+                        <p className="col">{`${`Q#${i + 1}`}    ${
+                          item.options1
+                        }`}</p>
+                        <Button className="float-right" size="small" color="primary" variant="outlined">
+                          Search Questions
+                        </Button>
+                      </div>
+                      <div className="row mt-2 mb-2">
+                        <p className="col">{`${`Q#${i + 1}`} ${
+                          item.options2
+                        }`}</p>
+
+                        <Button className="float-right" size="small" color="primary" variant="outlined">
+                          Search Questions
+                        </Button>
+                      </div>
+                      <div className="row mt-2 mb-2">
+                        <p className="col">{`${`Q#${i + 1}`} ${
+                          item.options3
+                        }`}</p>
+
+                        <Button className="float-right" size="small" color="primary" variant="outlined">
+                          Search Questions
+                        </Button>
+                      </div>
+                      <div className="row mt-2 mb-2">
+                        <p className="col">{`${`Q#${i + 1}`} ${
+                          item.options4
+                        }`}</p>
+
+                        <Button className="float-right" size="small" color="primary" variant="outlined">
+                          Search Questions
+                        </Button>
+                      </div>
                     </List>
-                  </Collapse>
-                </List>
+                  </Collapsible>
+                </div>
               );
             })}
           </Paper>
